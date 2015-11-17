9d54cfcd58f4ece9348949d8968fef0cc2d50d2b
lixu0104 <lixu1.2016@u.northwestern.edu>
2015-11-16 19:48:13 -0600

    finished withdraw and personal details

--- mysql_last.cpp
+++ mysql_last.cpp
@@ -1,12 +1,17 @@
 // mysql_last.cpp : 定义控制台应用程序的入口点。
 //
+
 #include <mysql.h>
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <string.h>
+#include <unordered_set>
 typedef unsigned int SOCKET;
 #ifndef my_socket_defined
+
 #define my_socket SOCKET
+
 #endif
 #include "mysql.h"
 using namespace std;
@@ -15,6 +20,16 @@ MYSQL *conn;
 class show {
 public:
     void starter(){
+        try{
+            char* sql = new char[100];
+            conn = mysql_init ( NULL );
+            sprintf(sql, "ALTER TABLE uosoffering ADD status VARCHAR(60)");
+            mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
+            mysql_query(conn, sql);
+        }
+        catch(int e){
+            cout << "already modified"<< endl;
+        }
         cout<<"1.student login"<<endl;
         cout<< ">>";
     }
@@ -35,7 +50,7 @@ public:
         int numrows = (int)mysql_num_rows(res_set);
         mysql_free_result( res_set );
         mysql_close( conn );
-        int result = 0;
+        int result;
         if (numrows == 1) {
             cout<<"login successful"<<endl;
             result = studentMenu();
@@ -56,9 +71,9 @@ public:
         int year = t_tm->tm_year + 1900;
         int month = t_tm->tm_mon + 1;
         int day = t_tm->tm_mday;
-        int quarter = getQuarter(day,month);
-        char* combine = new char[200];
-        sprintf(combine,"SELECT UoSCode, UoSName FROM unitofstudy join transcript using(UoSCode)  WHERE StudId = %d AND Grade is null;", userId);
+        int quarter = getQuarter(day, month);
+        char* combine = new char[1024];
+        sprintf(combine,"SELECT UoSCode, UoSName FROM unitofstudy join transcript using(UoSCode)  WHERE StudId = %d AND Grade is null AND Semester = 'Q%d' AND Year = '%d';", userId, quarter, year);
         conn = mysql_init ( NULL );
         mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
         MYSQL_RES *res_set;
@@ -68,7 +83,7 @@ public:
         res_set = mysql_store_result(conn);
         int numrows = (int)mysql_num_rows(res_set);
         // Display results
-        cout<<"UoSCode:     "<<"UoSName: "<<endl;
+        cout<<"CourseID:    "<<"CourseName: "<<endl;
         for (int i = 0; i < numrows; i++)
         {
             row = mysql_fetch_row( res_set );
@@ -89,29 +104,25 @@ public:
         bool pass = true;
         while (pass){
             cout<< ">>";
-            int choose;
+            int choose,result;
             cin>>choose;
-            if (cin.fail()) {
-                cin.clear();
-                while(cin.get() != '\n');
-            }
-            int result = 0;
             switch(choose) {
                 case 1:
                     cout<<"Transcript"<<endl;
                     result = transcript();
                     return result;
                 case 2:
-                    cout<<"Enroll"<<endl;
                     pass = false;
+                    Enroll();
                     break;
                 case 3:
                     cout<<"Withdraw"<<endl;
-                    pass = false;
-                    break;
+                    result = withdraw();
+                    return result;
                 case 4:
                     cout<<"Personal Details"<<endl;
-                    pass = false;
+                    result = updateDetails();
+                    return result;
                     break;
                 case 5:
                     cout<<"Logout"<<endl;
@@ -125,10 +136,11 @@ public:
     }
 
     int transcript(){
+        char courseNum[10];
+        conn = mysql_init ( NULL );
         mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
-        char* sql = new char[200];
-        sprintf(sql, "select UoSCode, Grade, UoSName from transcript inner join unitofstudy "
-                "using(UoSCode) where studid = %d;", userId);
+        char* sql = new char[1024];
+        sprintf(sql, "select UoSCode, Grade, UoSName from transcript inner join unitofstudy using(UoSCode) where studid = %d;", userId);
         int status = mysql_query(conn,sql);
         if(status){
             cout << "cannot connet to the mysql server";
@@ -205,14 +217,230 @@ public:
     }
 
     void Enroll() {
+        cout<<"Do u want to enroll in some courses? Y/N"<<endl;
+        string answer;
+        cin>>answer;
+        if (answer == "N") {
+            return ;
+        }
+        answer = "";
+        time_t timer;
+        time(&timer);
+        tm* t_tm = localtime(&timer);
+        int year = t_tm->tm_year + 1900;
+        int month = t_tm->tm_mon + 1;
+        int day = t_tm->tm_mday;
+        int quarter = getQuarter(day, month);
+        int followingquarter = quarter%4 + 1;
+        char* combine = new char[1024];
+        sprintf(combine,"SELECT UoSCode, Credits,Semester, UoSName FROM unitofstudy join whenoffered using(UoSCode) join uosoffering using(UoSCode, Semester) WHERE Semester = 'Q%d' OR Semester = 'Q%d' order by Semester;",quarter, followingquarter);
+        conn = mysql_init ( NULL );
+        mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
+        MYSQL_RES *res_set;
+        MYSQL_ROW row;
+        int a = mysql_query(conn,combine);//return int; 0 is true, 1 is false
 
+        res_set = mysql_store_result(conn);
+        int numrows = (int)mysql_num_rows(res_set);
+        unordered_set<string> xulihao2;
+        // Display results
+        cout<<"CourseID:  "<<"Credits:  "<<"Semester:  "<<"CourseName: "<<endl;
+        for (int i = 0; i < numrows; i++)
+        {
+            row = mysql_fetch_row( res_set );
+            if( row != NULL )
+            {
+                xulihao2.insert(row[0]);
+                cout << row[0] <<"   "<< row[1] <<"         "<< row[2]<<"         "<< row[3]<< endl;
+                xulihao2.insert(reverseCase(row[0]));
+            }
+        }
+        // free resources
+        cout<<"Which course? Please show us the CourseID"<<endl<<">>";
+        cin>>answer;
+        if (xulihao2.find(answer) != xulihao2.end()) {
+            cout<<"find it"<<endl;
+        }
+
+
+        mysql_free_result( res_set );
+        mysql_close( conn );
+    }
+
+    int withdraw() {
+        //print the courses the student has enrolled in with null grade
+        char* sql = new char[1024];
+        sprintf(sql, "select unitofstudy.uosname, transcript.uoscode, semester, year from transcript inner join unitofstudy using(uoscode) where grade is null and studid = %d;", userId);
+        conn = mysql_init ( NULL );
+        mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
+        MYSQL_RES *res_set;
+        MYSQL_ROW row;
+        int status = mysql_query(conn,sql);//return int; 0 is true, 1 is false
+        if(status){
+            cout << "cannot connet to the mysql server";
+            mysql_close(conn);
+            exit(0);
+        }
+        res_set = mysql_store_result(conn);
+        int numrows = (int)mysql_num_rows(res_set);
+        for (int i = 0; i < numrows; i++) {
+            row = mysql_fetch_row(res_set);
+            if (row != NULL) {
+                cout << "Course Id : " << row[1];
+                cout << " Course Name : " << row[0];
+                cout << " Semester: " << row[2];
+                cout << " Year: " << row[3] << endl;
+            }
+        }
+        mysql_free_result(res_set);
+        cout << "1.go back to student menu" << endl;
+        cout << "2.withdraw an enrolled class" << endl;
+        cout << ">>";
+        int choice;
+        cin >> choice;
+        if (cin.fail()) {
+            cout << "invalid input " << endl;
+            cin.clear();
+            while(cin.get() != '\n');
+        }
+        if(choice == 1){
+            return 1;
+        } else if (choice == 2){
+            string courseNum;
+            string semester;
+            time_t timer;
+            time(&timer);
+            tm* t_tm = localtime(&timer);
+            int year = t_tm->tm_year + 1900;
+            int month = t_tm->tm_mon + 1;
+            int day = t_tm->tm_mday;
+            int quarter = getQuarter(day, month);
+            int followingquarter = quarter%4 + 1;
+            int followingyear = quarter == 4? year+1:year;
+            cout << "please enter the course number:" << endl;
+            cout << ">>";
+            cin >> courseNum;
+            cout << "please enter semester of this course, e.g Q1:" << endl;
+            cout << ">>";
+            cin >> semester;
+            int semNum = checkStr(semester);
+            cout << " semNum value is: " << semNum << endl;
+            sprintf(sql, "select * from transcript where studid = %d and uoscode = '%s' and semester = '%s';", userId, courseNum.c_str(), semester.c_str());
+            status = mysql_query(conn,sql);
+            if(status){
+                cout << "course number might be wrong" << endl;
+                return 1;
+            }
+            res_set = mysql_store_result(conn);
+            row = mysql_fetch_row(res_set);
+            if (!row){
+                cout << "this course does not exist" << endl;
+                return 1;
+            }
+            if (quarter == semNum) {
+                cout << " year: " << year << "quarter :"<< quarter << endl;
+                sprintf(sql, "CALL withdraw_class('%s', '%s', %d, %d)", courseNum.c_str(), semester.c_str(), year, userId);
+                cout << "p1 break2" << endl;
+                status = mysql_query(conn,sql);
+                if(status){
+                    cout << " cannot connet to the mysql server";
+                    mysql_close(conn);
+                    exit(0);
+                }
+                res_set = mysql_store_result(conn);
+                mysql_free_result(res_set);
+                query_warning(courseNum, semester, year);
+            } else {
+                cout << " year: " << followingyear << "quarter :"<< followingquarter << endl;
+                sprintf(sql, "CALL withdraw_class('%s', '%s', %d, %d)", courseNum.c_str(), semester.c_str(), followingyear, userId);
+                status = mysql_query(conn,sql);
+                if(status){
+                    cout << " cannot connet to the mysql server";
+                    mysql_close(conn);
+                    exit(0);
+                }
+                res_set = mysql_store_result(conn);
+                mysql_free_result(res_set);
+                query_warning(courseNum, semester, followingyear);
+            }
+        } else {
+            cout << "you entered an invalid number, jumps to sutdent menu now" << endl;
+            return 1;
+        }
+        return 1;
+    }
+
+    int updateDetails(){
+        char* sql = new char[1024];
+        sprintf(sql, "select * from student where id = %d", userId);
+        conn = mysql_init ( NULL );
+        mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
+        MYSQL_RES *res_set;
+        MYSQL_ROW row;
+        int status = mysql_query(conn,sql);//return int; 0 is true, 1 is false
+        if(status){
+            cout << "cannot connet to the mysql server";
+            mysql_close(conn);
+            exit(0);
+        }
+        res_set = mysql_store_result(conn);
+        row = mysql_fetch_row(res_set);
+        cout << "id : " << row[0] << " name: " << row[1] << " password: " << row[2] << " address: " << row[3]<<endl;
+        cout << "1.go back to student menu" << endl;
+        cout << "2.update personal detail" << endl;
+        cout << ">>";
+        int choice;
+        cin >> choice;
+        if (cin.fail()) {
+            cout << "invalid input " << endl;
+            cin.clear();
+            while(cin.get() != '\n');
+        }
+        if(choice == 1){
+            return 1;
+        } else{
+            cout << "1.update password" << endl;
+            cout << "2.update address" << endl;
+            cout << ">>";
+            int update_choice;
+            cin >> update_choice;
+            if (update_choice == 1){
+                char* password = new char[100];
+                cout << "please enter new password:" << endl;
+                cout << ">>";
+                cin >> password;
+                sprintf(sql, "update student set password = '%s' where id = %d;", password, userId);
+                mysql_query(conn, sql);
+                cout << "password has been changed"<< endl;
+            } else if (update_choice == 2){
+                char* address = new char[100];
+                cout << "please enter new address:" << endl;
+                cout << ">>";
+                cin >> address;
+                sprintf(sql, "update student set address = '%s' where id = %d;", address, userId);
+                mysql_query(conn, sql);
+                cout << "address has been changed" << endl;
+            } else {
+                cout << "you entered an invalid number, jumps to sutdent menu now" << endl;
+                return 1;
+            }
+        }
+        return 1;
+    }
+
+    int checkStr(string input){
+        if (input.length() != 2){
+            return -1;
+        } else {
+            return atoi(input.substr(1,1).c_str());
+        };
     }
 
 private:
     int userId;
-    char courseNum[10];
     char password[10];
-    int getQuarter(int day, int month) {
+
+    int getQuarter(int day,int month) {
         if (month >= 9 && month <= 12) {
             return 1;
         }
@@ -239,6 +467,38 @@ private:
             }
         }
         return 4;
+    }
+
+    char* reverseCase(char* strings){
+        int length = strlen(strings);
+        for (int i=0; i<length; i++) {
+            if(strings[i] >= 'A' && strings[i] <= 'Z') {
+                strings[i] = strings[i] + 32;
+            }
+        }
+        return strings;
+    }
+
+    void query_warning(string courseNum, string semester, int year){
+        conn = mysql_init (NULL);
+        mysql_real_connect(conn, "localhost", "root", "103103", "project3-nudb", 0, NULL, 0);
+        MYSQL_RES *res_set;
+        MYSQL_ROW row;
+        char* sql = new char[1024];
+        char* status= new char[20];
+        sprintf(status,"warning");
+        sprintf(sql, "select status from uosoffering where uoscode = '%s' and semester = '%s' and year = %d;", courseNum.c_str(), semester.c_str(), year);
+        try {
+            mysql_query(conn, sql);//return int; 0 is true, 1 is false
+            res_set = mysql_store_result(conn);
+            row = mysql_fetch_row(res_set);
+            if (strcmp(row[0],"warning")==0){
+                cout << "the enrollment number of course " << courseNum << " is less than 50% of max enrollment: "  << endl;
+            }
+        }
+        catch(int e){
+            cout << "exception : "<< e << endl;
+        }
     }
 };
 
